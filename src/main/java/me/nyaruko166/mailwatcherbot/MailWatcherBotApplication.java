4803f7bda00b70970558f1c48fe7415a2fc189fd
Nyaruko166 <occho1666@gmail.com>
2024-12-03 11:02:02 +0700

    Init

---
+++ src/main/java/me/nyaruko166/mailwatcherbot/MailWatcherBotApplication.java
@@ -0,0 +1,13 @@
+package me.nyaruko166.mailwatcherbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MailWatcherBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MailWatcherBotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/nyaruko166/mailwatcherbot/MailWatcherBotApplicationTests.java
@@ -0,0 +1,13 @@
+package me.nyaruko166.mailwatcherbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MailWatcherBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
